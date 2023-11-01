a26ea1cc80facd10871eee06fa0ca38d19302840
Jefferson Ferreira <jeffersonferreiraborges@hotmail.com>
2023-10-31 23:46:39 -0300

    adição de arquivos 1051 e 1052

--- listas-beecrowd-sharp-coders/1051-imposto de_renda.js
+++ listas-beecrowd-sharp-coders/1051-imposto de_renda.js
@@ -12,12 +12,14 @@ let salario = parseFloat(lines.shift());
 let imposto = 0;
 
 if(salario > 0 && salario <= 2000.00){
-    imposto = salario * 0.8;
     console.log("Isento");
 }else if(salario >= 2000.01 && salario <= 3000.00){
-    console.log(`${imposto}`)
+    imposto = (salario - 2000.00) * 0.08;
+    console.log(`R$ ${imposto.toFixed(2)}`);
 }else if(salario > 3000.01 && salario <= 4500.00){
-    
+    imposto = (1000.00 * 0.08) + (salario - 3000.00) * 0.18;
+    console.log(`R$ ${imposto.toFixed(2)}`);
 }else if(salario >= 4500.00){
-    
+    imposto = (1000.00 * 0.08) + (1500.00 * 0.18) + (salario - 4500.00) * 0.28;
+    console.log(`R$ ${imposto.toFixed(2)}`);
 };
