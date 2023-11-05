c0ed2da4de44d92caae6735908e0fab71d664699
Jefferson Ferreira <jeffersonferreiraborges@hotmail.com>
2023-11-05 19:54:01 -0300

    adicionado exercício 1072

---
+++ listas-beecrowd-sharp-coders/1072-intervalo_2.js
@@ -0,0 +1,24 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+let N = parseInt(lines.shift());
+let dentro = 0;
+let fora = 0;
+
+for(let i = 0; i < N; i++){
+    let X = parseInt(lines.shift());
+    if(X >= 10 && X <= 20){
+        dentro++;
+    }else{
+        fora++;
+    }
+}
+
+console.log(`${dentro} in`);
+console.log(`${fora} out`);
