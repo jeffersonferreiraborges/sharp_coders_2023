1f1967739a830302f37539d89d96bc9e35623c27
Jefferson Ferreira <jeffersonferreiraborges@hotmail.com>
2023-11-05 21:09:04 -0300

    adicionado exercício 1075

---
+++ listas-beecrowd-sharp-coders/1075-resto_2.js
@@ -0,0 +1,16 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+let N = parseInt(lines.shift());
+
+for(let i = 1; i <= 10000; i++){
+    if(i % N == 2){
+        console.log(i);
+    }
+}
